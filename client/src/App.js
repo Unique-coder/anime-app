b57f03c88b0299d54c136f2b58c1d35f503d96f0
Unique-coder <Tom-Lesh@outlook.com>
2022-02-08 16:23:43 +0100

    Imported Material. Set up basic jsx file structure.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import Grow from "@mui/material/Grow";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+import Grid from "@mui/material/Grid";
+
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+
+import memories from "./images/memories.png";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            // alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
