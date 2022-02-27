534dfbcfe768f53e7c71547e8c78bc5d4d1466a0
Unique-coder <Tom-Lesh@outlook.com>
2022-02-27 19:04:20 +0100

    installed some components to create chip input. didnt work. Had to install sass. and use a different library. Created my own working input tag components.
    Helps: https://www.jinno.io/app/6/placeholder?source=react-tag-input,
    https://www.npmjs.com/package/react-tag-input

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,39 +1,127 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
-import { Grow, Container, Grid, Paper } from "@mui/material";
+import {
+  Container,
+  Grow,
+  Grid,
+  AppBar,
+  TextField,
+  Button,
+  Paper,
+} from "@mui/material";
+import { WithContext as ReactTags } from "react-tag-input";
 // import Container from "@mui/material/Container";
 // import Grid from "@mui/material/Grid";
-
+import { useNavigate, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import useStyles from "./styles";
+import styles from "./ReactTags.module.scss";
 
 import { getPosts } from "../../actions/post";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 import Pagination from "../Pagination";
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const query = useQuery();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
 
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
 
+  // const handleKeyPress = (e) => {
+  //   if (e.keycode === 13) {
+  //     // Search post
+  //   }
+  // };
+
+  const KeyCodes = {
+    comma: 188,
+    enter: 13,
+  };
+
+  const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
+  const handleAdd = (tag) => setTags([...tags, tag]);
+
+  const handleDelete = (tagToDelete) =>
+    setTags(tags.filter((tag, index) => index !== tagToDelete));
+
+  const handleDrag = (tag, currPos, newPos) => {
+    const newTags = tags.slice();
+
+    newTags.splice(currPos, 1);
+    newTags.splice(newPos, 0, tag);
+
+    // re-render
+    setTags(newTags);
+  };
+
+  const onClearAll = () => {
+    setTags([]);
+  };
+
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
           className={classes.mainContainer}
           container
           justify="space-between"
-          // alignItems="stretch"
+          alignItems="stretch"
           spacing={3}
         >
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              {/* <TextField
+                // onKeyDown={handleKeyPress}
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Chip
+                label="Search Tags"
+                value={tags}
+                style={{ margin: "10px 0" }}
+                variant="outlined"
+                onAdd={handleAdd}
+                onDelete={handleDelete}
+              /> */}
+              <div className={styles.ReactTags}>
+                <ReactTags
+                  tags={tags}
+                  handleDelete={handleDelete}
+                  handleAddition={handleAdd}
+                  handleDrag={handleDrag}
+                  delimiters={delimiters}
+                  onClearAll={onClearAll}
+                  placeholder="Search memories"
+                  inputFieldPosition="top"
+                  autocomplete
+                  clearAll={true}
+                />
+              </div>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper className={classes.pagination} elevation={6}>
               <Pagination />
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -1,16 +1,30 @@
 import { makeStyles } from "@material-ui/styles";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme();
 
 export default makeStyles(() => ({
   mainContainer: {
     flexDirection: "row",
     border: "1px solid blue",
-    gap: "150px",
+    // gap: "150px ",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: "column-reverse",
+    },
   },
   pagination: {
     display: "flex",
     flexDirection: "row",
+    justifyContent: "center",
     borderRadius: 4,
     marginTop: "1rem",
     padding: "16px",
+  },
+  appBarSearch: {
+    borderRadius: 4,
+    marginBottom: "1rem",
+    display: "flex",
+    padding: "16px",
+    justifyContent: "center",
   },
 }));
